af25cbe43651a363da3c7b17fdeac86ab3d02f33
Bini <software.bini30@gmail.com>
2026-06-23 21:11:33 +0300

    Add models for school app

--- backend/schools/models.py
+++ backend/schools/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    unique_code = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.name
